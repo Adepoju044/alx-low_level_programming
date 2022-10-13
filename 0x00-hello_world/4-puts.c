574285801f31ed3c22c2edbb21059100334a9216
Adepoju044 <113683907+Adepoju044@users.noreply.github.com>
2022-10-13 11:35:45 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,16 +4,7 @@
  * Return: Always 0
  */
 int main(void)
-{
-<<<<<<< HEAD
-	puts("\"programming is like building a multilingual puzzle");
-	return (0);
-}
-
-=======
+{ 
 	puts("\"programming is like builing a multiple puzzle");
-	return (0); 
-	
+	return (0);	
 {
-	
->>>>>>> 6450c9b6c745519c8860e4391787cc4d3d27ad1f

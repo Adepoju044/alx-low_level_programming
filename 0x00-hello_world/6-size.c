0a5e63b8a72e39332bc0721875664d9f59bb52a7
Adepoju044 <adepojufathia04@gmail.com>
2022-10-13 21:20:07 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-#include <stdio.h>
+
+#include < stdio.h >
 
 /**
  * main - print the size of various types
  * Description: prints the size  of various types on the computer
-=======
-#include <stdio.h> 
-
-/**
- * main - print the size of variouse types
- * Description: prints the size of various types on the computer it is complied
->>>>>>> 45db3cc4748e7bfcef21ecd1eabf973f42805991
  * and run on
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
-<<<<<<< HEAD
 	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
 	printf("Size of a long int: %d byte(s)\n," (int) sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long
@@ -26,12 +17,3 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n," (int) sizeof(float);
 			return (0);
 }
-
-=======
-	printf(Size of an int: %d byte(s)\n", (int) sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
-	return (0);
-	}
->>>>>>> 45db3cc4748e7bfcef21ecd1eabf973f42805991

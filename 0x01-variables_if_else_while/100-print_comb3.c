df516f8a226b519d3affa564b62a9071e4dd60d7
Adepoju044 <adepojufathia04@gmail.com>
2022-10-17 16:57:48 -0700

    16

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,19 @@ int main(void)
 	int ones = '0';
 	int tens = '0';
 
-	for (tens = '0'; tens <= '0'; tens++)/* prints tens digit*/
+	for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 	{
-		for (ones = '0'; ones <= '0'; ones++)/* prints ones digit*/
+		for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 		{
 			if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
 			{
+				putchar(tens);
+				putchar(ones);
+				if (!(ones  == '9' && tens == '8'))/*addes comma and space*/
+				{
 				putchar(',');
 				putchar(' ');
+				}	
 			}
 		}
 	}
